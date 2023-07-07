7659fdcbf558137574fc0ece2c00c17f4f1074d5
pabiel <panpabiel@gmail.com>
2023-07-07 23:52:02 +0000

    changes

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
 
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
@@ -26,3 +26,8 @@ ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+
+
